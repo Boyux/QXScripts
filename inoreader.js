2325bac43dcc0e46b0d5cdda39513caf9ff75de5
进击的嘿咻 <xuboyu72@gmail.com>
2020-12-16 14:34:31 +0800

    Add files via upload

---
+++ inoreader.js
@@ -0,0 +1,12 @@
+let data = JSON.parse($response.body);
+
+items = data["items"].map((element) => {
+    let content = element["summary"]["content"].replace(/<center>.*?<\/center>/m, "");
+    element["summary"]["content"] = content;
+    return element;
+})
+data["items"] = items;
+
+let body = JSON.stringify(data)
+
+$done({ body: body })
