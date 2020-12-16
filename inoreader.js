30a89fe06e4519a48e150a55dc994ac69ac09bb3
进击的嘿咻 <xuboyu72@gmail.com>
2020-12-16 16:35:11 +0800

    Update inoreader.js

--- inoreader.js
+++ inoreader.js
@@ -1,7 +1,7 @@
 let data = JSON.parse($response.body);
 
 items = data["items"].map((element) => {
-    let content = element["summary"]["content"].replace(/^.*?<center>.*?Ads from Inoreader.*?<\/center>/, "");
+    let content = element["summary"]["content"].replace(/<center>(.|\s)*?Ads from Inoreader(.|\s)*?<\/center>/, "");
     element["summary"]["content"] = content;
     console.log(content);
     return element;
